d3fd7f69e1ccd3f7afec13e3ca168dea24acf21b
azazo1 <azazo1@qq.com>
2025-01-29 00:12:17 +0800

    bench: count file lines

---
+++ benches/io_benches.rs
@@ -0,0 +1,16 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn count_file_lines() -> usize {
+    // 数 260w 行对应所有操作大约需要 360ms.
+    let buf_reader = BufReader::new(File::open("example_data/example_1.csv").unwrap());
+    buf_reader.lines().count()
+}
+
+fn bench_io(c: &mut Criterion) {
+    c.bench_function("count_file_lines", |b| b.iter(|| count_file_lines()));
+}
+
+criterion_group!(benches, bench_io);
+criterion_main!(benches);
